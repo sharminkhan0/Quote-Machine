a73d2841f57f3afa37b79c1a49973beca7fdf684
Sharmin Akter Khan <sharminakterkhan0@gmail.com>
2023-10-20 03:38:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const quotes = [
+    {
+        text: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs"
+    },
+    {
+        text: "Life is what happens when you're busy making other plans.",
+        author: "John Lennon"
+    },
+    {
+        text: "In three words I can sum up everything I've learned about life: it goes on.",
+        author: "Robert Frost"
+    }
+];
+
+function getRandomQuote() {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+function setRandomQuote() {
+    const quote = getRandomQuote();
+    document.getElementById("text").textContent = quote.text;
+    document.getElementById("author").textContent = `- ${quote.author}`;
+}
+
+document.getElementById("new-quote").addEventListener("click", setRandomQuote);
+
+setRandomQuote(); // Initial quote on page load
 
